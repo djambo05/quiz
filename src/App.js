d7431c79465f0fc859c9aea81b94e7b0a4323fad
Djamalutin <fisher.gerryy@mail.ru>
2023-05-09 11:37:53 +0300

    feature: building the application architecture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./index.scss";
 
+const Game = () => {
+  return (
+    <>
+      <div>
+        <div></div>
+      </div>
+      <h1></h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>
+  );
+};
 function App() {
   return <div className="App"></div>;
 }
